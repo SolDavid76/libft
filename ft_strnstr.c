82b6e3d0be40b798ef0e0deb9fe2a538ea03eae8
Dawid Janusz <djanusz@e2r7p22.clusters.42paris.fr>
2022-11-09 10:15:16 +0100

    ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,8 +6,31 @@
 /*   By: djanusz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 09:42:15 by djanusz           #+#    #+#             */
-/*   Updated: 2022/11/09 09:42:17 by djanusz          ###   ########.fr       */
+/*   Updated: 2022/11/09 10:14:34 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
+{
+	int	i;
+	int	j;
+
+	if(s2 == '\0')
+		return (s1);
+	i = 0;
+	while(s1[i] != '\0' && i < len)
+	{
+		j = 0;
+		while(s1[i] == s2[j] && i < len)
+		{
+			if(s2[j] == '\0')
+				return(s1[j]);
+			i++;
+			j++;
+		}
+		i++;
+	}
+	return(s1[j]);
+}
