8be612d2fe5f2f7dcdd13272417ce65e1a71895b
Dawid JANUSZ <djanusz@e3r12p18.clusters.42paris.fr>
2022-11-22 19:14:09 +0100

    OK

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: djanusz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:50:05 by djanusz           #+#    #+#             */
-/*   Updated: 2022/11/22 15:15:42 by djanusz          ###   ########.fr       */
+/*   Updated: 2022/11/22 19:12:25 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 void	*ft_calloc(size_t n, size_t size)
 {
-	void		*res;
+	size_t	total;
+	void	*res;
 
-	res = malloc(n * size);
+	total = n * size;
+	if (n != total / size)
+		return (NULL);
+	res = malloc(total);
 	if (!res)
 		return (NULL);
-	ft_bzero(res, n * size);
+	ft_bzero(res, total);
 	return (res);
 }
